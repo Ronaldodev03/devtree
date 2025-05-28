9b7fb66b90b7a693fac76f7e05d1586998969506
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2025-05-28 12:57:00 -0400

    Add auth middleware & getUser route & README

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -61,3 +61,7 @@ export const login = async (req: Request, res: Response) => {
 
   res.send(token);
 };
+
+export const getUser = async (req: Request, res: Response) => {
+  res.json(req.user);
+};
